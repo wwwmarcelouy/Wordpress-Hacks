3e89018c710b27c2e473b9c83314bbbad42a4418
wwwmarcelouy <marcelosuarezcastaldi@gmail.com>
2017-10-26 15:38:42 -0300

    Update admin-style.php

--- admin-style.php
+++ admin-style.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * REMOVE Entire Left Side Admin Menu and remove margin-left footer
  */
@@ -31,8 +32,29 @@ function admin_style() {
 }
 add_action( 'admin_enqueue_scripts', 'admin_style' );
 
+/**
+* View in https://speckyboy.com/20-snippets-and-hacks-to-make-wordpress-user-friendly-for-your-clients/
+**/
+
 //change the footer text
 function my_admin_footer_text () {
     echo '<img src="' . plugins_url( 'images/wptutsplus-icon.png' , __FILE__ ) . '">By <a href="https://marcelo.uym">INFOCUS</a>.';
 }
 add_filter( 'admin_footer_text', 'my_admin_footer_text' );
+
+// login page logo
+function custom_login_logo() {
+	echo '<style type="text/css">h1 a { background: url('.get_bloginfo('template_directory').'/companylogo.png) 50% 50% no-repeat !important; }</style>';
+}
+add_action('login_head', 'custom_login_logo');
+
+//hook the administrative header output
+add_action('admin_head', 'my_custom_logo');
+
+function my_custom_logo() {
+echo '
+<style type="text/css">
+#header-logo { background-image: url('.get_bloginfo('template_directory').'/images/custom-logo.gif) !important; }
+</style>
+';
+}
